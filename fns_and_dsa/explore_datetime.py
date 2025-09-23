afdd12e44424870b20bffbf093a73074f301bdb2
nobbonsu <nobbonsu440@gmail.com>
2025-09-23 23:28:40 +0000

    Exploring datetime in python

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+import time
+
+# dt = datetime.datetime.fromtimestamp(time.time())
+
+
+def display_current_datetime():
+    current_date = datetime.fromtimestamp(time.time())
+    print(f'Current date and time: {current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:{current_date.minute}:{current_date.second}')
+
+display_current_datetime()
+
+user_input = int(input("Enter a number of days: "))
+
+def calculate_future_date():
+    current_date = datetime.now()
+    future_date = current_date.date() - timedelta(user_input)
+    print(future_date)
+
+calculate_future_date()
+
