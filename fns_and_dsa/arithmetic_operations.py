8e9ea8f38ab60aed42609372baa73a063202bc4f
nobbonsu <nobbonsu440@gmail.com>
2025-09-23 15:17:33 +0000

    arithmetic operation updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2 ,operation):
     possible_operation_values = ['add', 'subtract', 'multipy', 'divide']
 
     match operation:
