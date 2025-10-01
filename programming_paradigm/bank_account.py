5392c6f19669955e1643d540fcc240677d092942
nobbonsu <nobbonsu440@gmail.com>
2025-10-01 17:14:08 +0000

    Paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,5 @@ class BankAccount:
         else:
             return False
             
-            
-
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
