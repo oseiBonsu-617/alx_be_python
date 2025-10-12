8450b8a4621df31e0d135620d580b47f80567378
nobbonsu <nobbonsu440@gmail.com>
2025-10-12 20:58:33 +0000

    library system updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,7 +1,9 @@
 class Book:
-    def __init__(self, title: str, author: str):
-        self.title = title
-        self.author = author
+    def __init__(self, title: str, author: str, description: str, category: str):
+        self.title = title.lower()
+        self.author = author.lower()
+        self.description = description.lower()
+        self.category = category.lower()
 
     def __str__(self):
         return f"{self.__class__.__name__}: {self.title} by {self.author}"
@@ -33,9 +35,95 @@ class Library:
     def __init__(self):
         self.books = []
 
+    def check_book_shelf(self):
+        if not self.books:
+            print("Book Shelf is empty.")
+            return 
+
+
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
             print(book)
+
+    def remove_book(self, title: str):
+        self.check_book_shelf()
+        
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                print(f"'{title}' has been removed from the library.")
+                return
+        
+        print(f"No book found with title '{title}'.")
+
+    def search_by_author(self, author: str):
+        self.check_book_shelf()
+        
+        for book in self.books:
+            if book.author == author:
+                print(book)
+                return
+        print(f"No book found with title '{author}'.")
+
+    def search_by_title(self, title: str):
+        self.check_book_shelf()
+
+        book_search_suggestions = []
+        
+        for book in self.books:
+            if title.lower() in book.title.lower() or title.lower() in book.author.lower():
+                book_search_suggestions.append(book)
+
+        if not book_search_suggestions:
+            print("No books found")
+        else:
+            for i in book_search_suggestions:
+                print(i)
+
+    def search_by_keyword(self, keyword: str, mode="or"):
+        self.check_book_shelf()
+        
+        terms = [term.strip().lower() for term in keyword.split(" ")]
+
+        results = []
+
+        for book in self.books:
+            if mode == "or":
+                if any(term in book.title or term in book.author or term in book.description for term in terms):
+                    results.append(book)
+
+            else:
+                if any(term in book.title and term in book.author and term in book.description for term in terms):
+                    results.append(book)
+
+        return results
+
+    def search_by_category(self, category: str):
+        self.check_book_shelf()
+        
+        for book in self.books:
+            if category in book.category:
+                print(book)
+                return
+            
+        print(f"No book found with category '{category}'.")
+
+    
+
+    def search(self, *, title=None, author=None, keywords=None, category=None, mode="or"):
+        self.check_book_shelf()
+
+        results = []
+
+        for book in self.books:
+            if mode == "or":
+                if any(self.search_by_title(title) or self.search_by_keyword(keywords) or self.search_by_author or self.search_by_category(category)):
+                    results.append(book)
+            else:
+                if any(self.search_by_title(title) and self.search_by_keyword(keywords) and self.search_by_author and self.search_by_category(category)):
+                    results.append(book)
+
+        return results
--- oop/main.py
+++ oop/main.py
@@ -1,13 +1,36 @@
-from class_static_methods_demo import Calculator
+from library_system import Book, EBook, PrintBook, Library
 
 def main():
-    # Using the static method
-    sum_result = Calculator.add(10, 5)
-    print(f"The sum is: {sum_result}")
+    # Create a Library instance
+    my_library = Library()
 
-    # Using the class method
-    product_result = Calculator.multiply(10, 5)
-    print(f"The product is: {product_result}")
+    # Create instances of each type of book
+    classic_book = Book("Pride and Prejudice", "Jane Austen", "A Beautiful love Story", "Romance")
+    # digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    # paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+    # classic_book1 = Book("Pride and Prejudice 2", "Jane Austen")
+    # classic_book2 = Book("Snow white and Prejudice", "Jane Austen")
+
+    # Add books to the library
+    my_library.add_book(classic_book)
+    # my_library.add_book(classic_book1)
+    # my_library.add_book(classic_book2)
+    # my_library.add_book(digital_novel)
+    # my_library.add_book(paper_novel)
+
+    # List all books in the library
+    my_library.list_books()
+
+    # # Remove one book from the library
+    my_library.remove_book("Pride and Prejudice")
+
+    # #List all books in the Library
+    # my_library.list_books()
+
+    # # Search for a book in the library
+    # my_library.search_book("Jane Austen")
+
+    # my_library.search_by_title("Snow")
 
 if __name__ == "__main__":
     main()
